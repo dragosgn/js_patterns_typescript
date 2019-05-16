8cdf66b1ff3b3c2b82cf5008e5731d6d167c78c8
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-05-16 17:23:01 +0200

    observer pattern

---
+++ src/addyOsmani/theObserverPattern.js
@@ -0,0 +1,20 @@
+// The Observer is a design pattern where an object (known as a subject)
+// maintains a list of objects depending on it (observers), automatically notifying them of any changes to state.
+
+function ObserverList() {
+  this.observerList = [];
+}
+
+ObserverList.prototype.add = function(obj) {
+  return this.observerList.push(obj);
+};
+
+ObserverList.prototype.count = function() {
+  return this.observerList.length;
+};
+
+ObserverList.prototype.get = function(index) {
+  if (index > -1 && index < this.observerList.length) {
+    return this.observerList[index];
+  }
+};
